7ab6e301bc0b0c9a79ffc75b5dd6a8c6b68bb3a7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-09-28 11:11:44 +0200

    BadgeView

--- XamarinCommunityToolkitSample/ViewModels/Views/ViewsGalleryViewModel.cs
+++ XamarinCommunityToolkitSample/ViewModels/Views/ViewsGalleryViewModel.cs
@@ -7,8 +7,10 @@ namespace Xamarin.CommunityToolkit.Sample.ViewModels.Views
 {
 	public class ViewsGalleryViewModel : BaseViewModel
 	{
-		public IEnumerable<SectionModel> Items { get; } = new List<SectionModel> {
+		public IEnumerable<SectionModel> Items { get; } = new List<SectionModel>
+		{
 			new SectionModel(typeof(AvatarViewPage), AppResources.AvatarViewTitle, AppResources.AvatarViewDescription),
+			new SectionModel(typeof(BadgeViewPage), AppResources.BadgeViewTitle, AppResources.BadgeViewDescription),
 			new SectionModel(typeof(RangeSliderPage), AppResources.RangeSliderTitle, AppResources.RangeSliderDescription),
 			new SectionModel(typeof(SideMenuViewPage), AppResources.SideMenuViewTitle, AppResources.SideMenuViewDescription),
 			new SectionModel(typeof(CameraViewPage), AppResources.CameraViewTitle, AppResources.CameraViewDescription),
